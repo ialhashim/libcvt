9e7779c2b78d825fa2126acd8689d459881dfa67
Chenming Wu <wcm15@mails.tsinghua.edu.cn>
2017-03-15 15:55:05 +0800

    OFF mesh support

--- libcvt/cvt.h
+++ libcvt/cvt.h
@@ -24,21 +24,19 @@ namespace libcvd{
 			c[0] = x; c[1] = y; c[2] = z;
 			vertColor.push_back(c);
 		}
-<<<<<<< HEAD
 
-		void load_obj(std::string filename) {
+		bool loadOFF(std::string filename) {
 			using namespace std;
-			V.clear();
-			F.clear();
-			N.clear();
-			C.clear();
-
+			FILE* off_file;
+			errno_t err = fopen_s(&off_file, filename.c_str(), "r");
+			if (err != 0) return false;
 			// First line is always OFF
 			char header[1000];
 			const std::string OFF("OFF");
 			const std::string NOFF("NOFF");
 			const std::string COFF("COFF");
-			if (fscanf(off_file, "%s\n", header) != 1
+			std::cout << "hello" << std::endl;
+			if (std::fscanf(off_file, "%s\n", header) != 1
 				|| !(
 					string(header).compare(0, OFF.length(), OFF) == 0 ||
 					string(header).compare(0, COFF.length(), COFF) == 0 ||
@@ -63,13 +61,14 @@ namespace libcvd{
 				still_comments = line[0] == '#';
 			}
 			sscanf(line, "%d %d %d", &number_of_vertices, &number_of_faces, &number_of_edges);
-			V.resize(number_of_vertices);
+			verts.resize(number_of_vertices);
+			faces.resize(number_of_faces);
 			if (has_normals)
-				N.resize(number_of_vertices);
+				;// N.resize(number_of_vertices);
 			if (has_vertexColors)
-				C.resize(number_of_vertices);
-			F.resize(number_of_faces);
-			//printf("%s %d %d %d\n",(has_normals ? "NOFF" : "OFF"),number_of_vertices,number_of_faces,number_of_edges);
+				;// C.resize(number_of_vertices);
+			
+			printf("%s %d %d %d\n",(has_normals ? "NOFF" : "OFF"),number_of_vertices,number_of_faces,number_of_edges);
 			// Read vertices
 			for (int i = 0; i < number_of_vertices;)
 			{
@@ -77,30 +76,28 @@ namespace libcvd{
 				double x, y, z, nx, ny, nz;
 				if (sscanf(line, "%lg %lg %lg %lg %lg %lg", &x, &y, &z, &nx, &ny, &nz) >= 3)
 				{
-					std::vector<Scalar > vertex;
-					vertex.resize(3);
+					std::vector<double> vertex(3);
 					vertex[0] = x;
 					vertex[1] = y;
 					vertex[2] = z;
-					V[i] = vertex;
+					verts[i] = vertex;
 
 					if (has_normals)
 					{
-						std::vector<Scalar > normal;
-						normal.resize(3);
+						std::vector<double> normal(3);
 						normal[0] = nx;
 						normal[1] = ny;
 						normal[2] = nz;
-						N[i] = normal;
+						//N[i] = normal;
 					}
 
-					if (has_vertexColors)
+					/*if (has_vertexColors)
 					{
 						C[i].resize(3);
 						C[i][0] = nx / 255.0;
 						C[i][1] = ny / 255.0;
 						C[i][2] = nz / 255.0;
-					}
+					}*/
 					i++;
 				}
 				else if (
@@ -123,7 +120,7 @@ namespace libcvd{
 			// Read faces
 			for (int i = 0; i < number_of_faces;)
 			{
-				std::vector<Index > face;
+				std::vector<int> face;
 				int valence;
 				if (fscanf(off_file, "%d", &valence) == 1)
 				{
@@ -141,7 +138,7 @@ namespace libcvd{
 
 						face[j] = index;
 					}
-					F[i] = face;
+					faces[i] = face;
 					i++;
 				}
 				else if (
@@ -160,8 +157,6 @@ namespace libcvd{
 			fclose(off_file);
 			return true;
 		}
-=======
->>>>>>> 7ae14886754d9e8aa0b6c81c7bfdd5c1d84f41f0
     };
 
     void computeCVT(SimpleMesh & m, int regions = 20, int iterations = 200)
